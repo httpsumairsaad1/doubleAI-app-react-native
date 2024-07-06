216f61161d8785af796de2d1c43e1202c92a095d
Umair Saad <umairsaad52@gmail.com>
2024-07-07 00:56:01 +0500

    DoubleAI

---
+++ app/_layout.tsx
@@ -0,0 +1,11 @@
+import { Slot } from 'expo-router'
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const RootLayout = () => {
+  return (
+    <Slot/>
+  )
+}
+
+export default RootLayout
