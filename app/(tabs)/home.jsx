50e7738d59e123b3b0a1138309a18b61a5137ac6
Umair Saad <umairsaad52@gmail.com>
2024-07-15 03:35:20 +0500

    latest post > home screen
    trending post remaining

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -5,31 +5,32 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 import { images } from "../../constants"; 
 import SearchInput from '../../components/SearchInput';
 import Trending from '../../components/Trending';
+import VideoCard from "../../components/VideoCard"
 import EmptyState from '../../components/EmptyState';
 import { getAllPosts } from "../../lib/appwrite"
 import useAppwrite from "../../lib/useAppwrite";
 
-Alert
-
 const Home = () => {
-
 const { data: posts, refetch } = useAppwrite(getAllPosts);
-
 const [refreshing, setRefreshing] = useState(false);
 
   const onRefresh = async () => {
     setRefreshing(true);
-    // await refetch();
+    await refetch();
     setRefreshing(false);
   };
+
+  // console.log(posts);
+
   return (
     <SafeAreaView className="bg-primary h-full">
       <FlatList
-        data={[{ id: 1 }, { id: 2 }, { id: 3 }]}
+        data={posts}
         // data={[]}
         keyExtractor={(item) => item.$id}
           renderItem={({ item }) => (
-            <Text className="text-3xl">{item.title}</Text>
+            // <Text className="text-3xl text-white">{item.title}</Text>
+            <VideoCard video={item} />
           )}
         ListHeaderComponent={() => (
           <View className="flex my-6 px-4 space-y-6">
---
+++ components/VideoCard.jsx
@@ -0,0 +1,68 @@
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import React from 'react'
+// import { Video } from "expo-av";
+import { useState } from "react";
+import {icons} from "../constants"
+
+const VideoCard = ({ video: { title, thumbnail, video, creator: {username, avatar}}}) => {
+
+    const [play, setPlay] = useState(false);
+
+  return (
+    <View className="flex flex-col items-center px-4 mb-14" >
+      <View className="flex flex-row gap-3 items-start">
+        <View className="flex justify-center items-center flex-row flex-1">
+          <View className="w-[46px] h-[46px] rounded-lg border border-secondary flex justify-center items-center p-0.5">
+                <Image
+                    source={{ uri: avatar }}
+                    className="w-full h-full rounded-lg"
+                    resizeMode="cover"
+                />
+          </View>
+          <View className="flex justify-center flex-1 ml-3 gap-y-1">
+
+            {/* video title */}
+            <Text className="font-psemibold text-sm text-white" numberOfLines={1}>
+              {title}
+            </Text>
+
+            {/* creator name */}
+            <Text className="text-xs text-gray-100 font-pregular" numberOfLines={1}>
+              {username}
+            </Text>
+
+          </View>
+        </View>
+
+        <View className="pt-2">
+          <Image source={icons.menu} className="w-5 h-5" resizeMode="contain" />
+        </View>
+      </View>    
+
+      {/* SHOWING VIDEO HERE */}
+      {play ? (
+        <Text>Playing</Text>
+        ) : (
+            <TouchableOpacity
+              activeOpacity={0.7}
+              onPress={() => setPlay(true)}
+              className="w-full h-60 rounded-xl mt-3 relative flex justify-center items-center"
+            >
+              <Image
+                source={{ uri: thumbnail }}
+                className="w-full h-full rounded-xl mt-3"
+                resizeMode="cover"
+              />
+    
+              <Image
+                source={icons.play}
+                className="w-12 h-12 absolute"
+                resizeMode="contain"
+              />
+            </TouchableOpacity>
+          )}
+    </View>
+  )
+}
+
+export default VideoCard
